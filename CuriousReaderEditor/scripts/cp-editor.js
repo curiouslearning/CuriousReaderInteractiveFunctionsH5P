823114d71ceebcedae8d8f894efadacd9bbc1e91
Rakshith <62013445+rakshith-arch@users.noreply.github.com>
2021-09-27 13:10:31 +0530

    changes

--- CuriousReaderEditor/scripts/cp-editor.js
+++ CuriousReaderEditor/scripts/cp-editor.js
@@ -1730,19 +1730,22 @@ H5PEditor.CuriousReader.prototype.addToDragNBar = function (element, elementPara
 
   if (type === "H5P.Image") {
     var text = '';
-    if ( H5P.jQuery('.h5p-current').find('#sentence-style').length > 0) {
-      H5P.jQuery('.h5p-current').find('#sentence-style').each(function() {
+    console.log(H5P.jQuery('.h5p-current'))
+    console.log( H5P.jQuery('.h5p-current').find('#sentence-style'))
+    // if ( H5P.jQuery('.h5p-current').find('#sentence-style').length > 0) {
+    //   console.log("Entered 1st if condition")
+    //   H5P.jQuery('.h5p-current').find('#sentence-style').each(function() {
        
-        var addedTextElement = H5P.jQuery(this);
-        var audioElementId=(addedTextElement[0].children[0]!=undefined)?H5P.jQuery(addedTextElement[0].children[0])[0].children[0].id:'';
-        var parentElementId = audioElementId.substr(0,audioElementId.length-1)
-        for (let i = 0; i < addedTextElement[0].children.length; i++) {
-          var splittedText = H5P.jQuery(addedTextElement[0].children[i])[0].innerHTML;
-          text = text + '<a href="#" id = img' + parentElementId + i + '>' + H5P.jQuery(splittedText).html() + '</a>'
-        }
-      })
-    }
-    var linkEle = '<div class="h5p-dragnbar-context-menu-button dropdown dropbtn linkText" role="button" tabindex="0" aria-label="LinkText"><div class="dropdown-content">'+text+'</div></div>'
+    //     var addedTextElement = H5P.jQuery(this);
+    //     var audioElementId=(addedTextElement[0].children[0]!=undefined)?H5P.jQuery(addedTextElement[0].children[0])[0].children[0].id:'';
+    //     var parentElementId = audioElementId.substr(0,audioElementId.length-1)
+    //     for (let i = 0; i < addedTextElement[0].children.length; i++) {
+    //       var splittedText = H5P.jQuery(addedTextElement[0].children[i])[0].innerHTML;
+    //       text = text + '<a href="#" id = img' + parentElementId + i + '>' + H5P.jQuery(splittedText).html() + '</a>'
+    //     }
+    //   })
+    // }
+    var linkEle = '<div class="h5p-dragnbar-context-menu-button dropdown dropbtn linkText" role="button" tabindex="0" aria-label="LinkText"><div class="dropdown-content"></div></div>'
     var link = dnbElement.contextMenu.$buttons.append(linkEle);
     link[0].children[5].addEventListener('click', function (e) { 
       var textId = e.target.id
@@ -1751,6 +1754,28 @@ H5PEditor.CuriousReader.prototype.addToDragNBar = function (element, elementPara
       H5P.jQuery(this).find('.dropdown-content').css({ 'display': 'none' })
     });
     H5P.jQuery(link[0].children[5]).mouseenter(function () {
+      if ( H5P.jQuery('.h5p-current').find('#sentence-style').length > 0) {
+        console.log("Entered 1st if condition")
+        H5P.jQuery('.h5p-current').find('#sentence-style').each(function() {
+         
+          var addedTextElement = H5P.jQuery(this);
+          var audioElementId=(addedTextElement[0].children[0]!=undefined)?H5P.jQuery(addedTextElement[0].children[0])[0].children[0].id:'';
+          var parentElementId = audioElementId.substr(0,audioElementId.length-1)
+          for (let i = 0; i < addedTextElement[0].children.length; i++) {
+            var splittedText = H5P.jQuery(addedTextElement[0].children[i])[0].innerHTML;
+            text = text + '<a href="#" id = img' + parentElementId + i + '>' + H5P.jQuery(splittedText).html() + '</a>'
+          }
+        })
+      }
+      console.log(H5P.jQuery(link[0].children[5].children[0]).length)
+      var i=0
+      if( H5P.jQuery(link[0].children[5].children[0]).length==1 && i==0)
+      {
+        console.log("Length is 0")
+      H5P.jQuery(link[0].children[5].children[0]).append(text)
+      i++
+      }
+     
       H5P.jQuery(this).find('.dropdown-content').css({ 'display': 'block' })
       H5P.jQuery(this).find('#'+elementParams.id).css({
         'background-color' : 'yellow'
