de8893ab5da9c26eb19b6a2355a9f8d384d3b2d9
george dzavashvili <dzavashviligeorge@gmail.com>
2022-01-05 12:28:56 +0400

    Add a separate initialization method to waveform
    - And remove the JQuery ready

--- h5p-editor-waveform/scripts/WaveformInit.js
+++ h5p-editor-waveform/scripts/WaveformInit.js
@@ -20,180 +20,182 @@ let WaveformInit = function (parent, field, params, setValue) {
   this.endTime = this.parent.params.endDuration != undefined ? this.parent.params.endDuration : 0.2;
   this.audioDuration;
   var self = this;
-
-  $(document).ready(() => {
-    // making word textfield disable for user so they can't enter using keyboard
-    // parent.children[1].$input[0].setAttribute('disabled',true);
-    if (!self.container || (self.container && self.container.length == 0)) return;
-    console.log("Waveform ready: ");
-    console.log(self);
-    var wavesurfer = WaveSurfer.create({
-      container: self.container[0],
-      waveColor: 'green',
-      progressColor: 'grey',
-      fillParent: true,
-      responsive: true,
-      barHeight: 8,
-      plugins: [
-        RegionsPlugin.create({
-          regionsMinLength: 0.1,
-          maxRegions: 1,
-          regions: [{
-            start: self.startTime,
-            end: self.endTime,
-            loop: false,
-            color: 'rgba(250, 203, 110, 0.5)'
-          }],
-          dragSelection: {
-            slop: 5
-          }
-        })
-      ]
-    });
-
-    $('.wavesurfer-handle').css("width", "4px");
-    $('.wavesurfer-handle').css("background-color", "#707070");
-
-    let region;
-    self.crAudioIndex = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields.length;
-    // let path = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex- 1].params.files ? H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex - 1].params.files[0].path : undefined;
-    let id = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[0].parent.params.subContentId;
-    let path = self.audioParams.files ? self.audioParams.files[0].path : undefined;
-    // let id =  self.parent.parent.parent.params.subContentId;
-    if (path != undefined && id != undefined) {
-      let file = H5P.getPath(path, id);
-      $.get(file).done(function () {
-        setTimeout(function () {
-          wavesurfer.load(file);
-        }, 1000)
-      }).fail(function () {
-        let id = H5PEditor.contentId;
-        let file = H5P.getPath(path, id);
-        setTimeout(function () {
-          wavesurfer.load(file);
-        }, 1000)
-      })
-    }
-
-
-    wavesurfer.on('ready', function () {
-      region = Object.values(wavesurfer.regions.list)[0];
-      let width = self.parent.parent.parent.parent.cp.width + (self.parent.parent.parent.parent.cp.width * 0.25);
-      self.audioDuration = wavesurfer.getDuration();
-      // wavesurfer.params.minPxPerSec = width / wavesurfer.getDuration();
-      // wavesurfer.drawBuffer();
-
-      // let regionId = self.id + "playRegion"
-      // let $playRegionButton = '<button id = '+ regionId +' class = "playRegion">Play</button>'
-      // $('#' + self.id).find('.wavesurfer-region').append($playRegionButton)
-      // $('#' + regionId).on('click', function (e) {
-      //   e.stopPropagation()
-      //   if (region != undefined) {
-      //     region.play()
-      //   }
-      // })
-    });
-
-    $(self.container).parent().parent().find('.field-name-startDuration').find('input').focusout(function (e) {
-      if (region != undefined) {
-        let value = e.target.value;
-        if (!isNaN(value)) {
-          if (parseFloat(value) > self.audioDuration) {
-            value = 0.0;
-          }
-          let inputStartTime = parseFloat(value);
-          let inputEndTime = region.end <= parseFloat(value) ? parseFloat(value) + 0.2 : region.end;
-          params = {
-            start: inputStartTime.toFixed(4),
-            end: inputEndTime.toFixed(4)
-          }
-          region.update(params)
-        } else {
-          $(this).parent().find('.h5p-errors').append("<p>The entered value must be Number not alphabet</p>")
-        }
-      }
-    });
-
-    $(self.container).parent().parent().find('.field-name-endDuration').find('input').focusout(function (e) {
-      if (region != undefined) {
-        let value = e.target.value;
-        if (!isNaN(value)) {
-          if (parseFloat(value) > self.audioDuration) {
-            value = self.audioDuration - 0.05;
-          }
-          let inputStartTime = parseFloat(value) <= region.start ? 0 : region.start
-          let inputEndTime = parseFloat(value);
-          params = {
-            start: inputStartTime.toFixed(4),
-            end: inputEndTime.toFixed(4)
-          }
-          region.update(params)
-        } else {
-          $(this).parent().find('.h5p-errors').append("<p>The entered value must be Number not alphabet</p>")
-        }
-      }
-    });
-
-    wavesurfer.on('region-updated', (event) => {
-      this.start = event.start;
-      this.end = event.end;
-      this.$startinput = $('#' + this.id).parent().parent().find('.field-name-startDuration').find('input');
-      this.$endinput = $('#' + this.id).parent().parent().find('.field-name-endDuration').find('input')
-      this.$startinput.val(this.start.toFixed(4));
-      this.$endinput.val(this.end.toFixed(4));
-      this.setValue(this.findField("startDuration", this.parent.field.fields), "" + this.start.toFixed(4));
-      this.setValue(this.findField("endDuration", this.parent.field.fields), "" + this.end.toFixed(4));
-    });
-
-    $(self.container).parents('.h5p-craudio-editor').find(".h5p-add-file").first().parent().find('ul').on('DOMSubtreeModified',
-      () => {
-        // let path = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex- 1].params.files ? H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex - 1].params.files[0].path : undefined;
-        let id = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex - 1].parent.params.subContentId;
-        let path = self.audioParams.files ? self.audioParams.files[0].path : undefined;
-        if (path != undefined && id != undefined) {
-          let file = H5P.getPath(path, id);
-          $.get(file).done(function () {
-            setTimeout(function () {
-              wavesurfer.load(file);
-            }, 1000)
-          }).fail(function () {
-            let id = H5PEditor.contentId;
-            let file = H5P.getPath(path, id);
-            setTimeout(function () {
-              wavesurfer.load(file);
-            }, 1000)
-          })
-        }
-        if (region != undefined) {
-          let $startinput = $('#' + this.id).parent().parent().find('.field-name-startDuration').find('input');
-          let $endinput = $('#' + this.id).parent().parent().find('.field-name-endDuration').find('input')
-          $startinput.val(0);
-          $endinput.val(0.2);
-          this.setValue(this.findField("startDuration", this.parent.field.fields), "" + 0);
-          this.setValue(this.findField("endDuration", this.parent.field.fields), "" + 0.2);
-          params = {
-            start: 0,
-            end: 0.2
-          }
-          region.update(params)
-        }
-      });
-
-    if (this.id != null) {
-      let regionId = this.id + "playRegion";
-      let $playRegionButton = $('<button id = ' + regionId + ' class = "playRegion">Play</button>');
-      $(self.container).parent('div').append($playRegionButton);
-      $($playRegionButton).on('click', function () {
-        if (region != undefined) {
-          region.play();
-        }
-      })
-    }
-  });
 }
 
 WaveformInit.prototype = Object.create(Parent.prototype);
 WaveformInit.prototype.constructor = WaveformInit;
+
+/**
+ * Initialize the waveform editor.
+ * @param {*} $wrapper 
+ */
+WaveformInit.prototype.init = function () {
+  if (!self.container || (self.container && self.container.length == 0)) return;
+  console.log("Waveform ready: ");
+  console.log(self);
+  var wavesurfer = WaveSurfer.create({
+    container: self.container[0],
+    waveColor: 'green',
+    progressColor: 'grey',
+    fillParent: true,
+    responsive: true,
+    barHeight: 8,
+    plugins: [
+      RegionsPlugin.create({
+        regionsMinLength: 0.1,
+        maxRegions: 1,
+        regions: [{
+          start: self.startTime,
+          end: self.endTime,
+          loop: false,
+          color: 'rgba(250, 203, 110, 0.5)'
+        }],
+        dragSelection: {
+          slop: 5
+        }
+      })
+    ]
+  });
+
+  $('.wavesurfer-handle').css("width", "4px");
+  $('.wavesurfer-handle').css("background-color", "#707070");
+
+  let region;
+  self.crAudioIndex = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields.length;
+  // let path = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex- 1].params.files ? H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex - 1].params.files[0].path : undefined;
+  let id = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[0].parent.params.subContentId;
+  let path = self.audioParams.files ? self.audioParams.files[0].path : undefined;
+  // let id =  self.parent.parent.parent.params.subContentId;
+  if (path != undefined && id != undefined) {
+    let file = H5P.getPath(path, id);
+    $.get(file).done(function () {
+      setTimeout(function () {
+        wavesurfer.load(file);
+      }, 1000)
+    }).fail(function () {
+      let id = H5PEditor.contentId;
+      let file = H5P.getPath(path, id);
+      setTimeout(function () {
+        wavesurfer.load(file);
+      }, 1000)
+    })
+  }
+
+
+  wavesurfer.on('ready', function () {
+    region = Object.values(wavesurfer.regions.list)[0];
+    let width = self.parent.parent.parent.parent.cp.width + (self.parent.parent.parent.parent.cp.width * 0.25);
+    self.audioDuration = wavesurfer.getDuration();
+    // wavesurfer.params.minPxPerSec = width / wavesurfer.getDuration();
+    // wavesurfer.drawBuffer();
+
+    // let regionId = self.id + "playRegion"
+    // let $playRegionButton = '<button id = '+ regionId +' class = "playRegion">Play</button>'
+    // $('#' + self.id).find('.wavesurfer-region').append($playRegionButton)
+    // $('#' + regionId).on('click', function (e) {
+    //   e.stopPropagation()
+    //   if (region != undefined) {
+    //     region.play()
+    //   }
+    // })
+  });
+
+  $(self.container).parent().parent().find('.field-name-startDuration').find('input').focusout(function (e) {
+    if (region != undefined) {
+      let value = e.target.value;
+      if (!isNaN(value)) {
+        if (parseFloat(value) > self.audioDuration) {
+          value = 0.0;
+        }
+        let inputStartTime = parseFloat(value);
+        let inputEndTime = region.end <= parseFloat(value) ? parseFloat(value) + 0.2 : region.end;
+        params = {
+          start: inputStartTime.toFixed(4),
+          end: inputEndTime.toFixed(4)
+        }
+        region.update(params)
+      } else {
+        $(this).parent().find('.h5p-errors').append("<p>The entered value must be Number not alphabet</p>")
+      }
+    }
+  });
+
+  $(self.container).parent().parent().find('.field-name-endDuration').find('input').focusout(function (e) {
+    if (region != undefined) {
+      let value = e.target.value;
+      if (!isNaN(value)) {
+        if (parseFloat(value) > self.audioDuration) {
+          value = self.audioDuration - 0.05;
+        }
+        let inputStartTime = parseFloat(value) <= region.start ? 0 : region.start
+        let inputEndTime = parseFloat(value);
+        params = {
+          start: inputStartTime.toFixed(4),
+          end: inputEndTime.toFixed(4)
+        }
+        region.update(params)
+      } else {
+        $(this).parent().find('.h5p-errors').append("<p>The entered value must be Number not alphabet</p>")
+      }
+    }
+  });
+
+  wavesurfer.on('region-updated', (event) => {
+    this.start = event.start;
+    this.end = event.end;
+    this.$startinput = $('#' + this.id).parent().parent().find('.field-name-startDuration').find('input');
+    this.$endinput = $('#' + this.id).parent().parent().find('.field-name-endDuration').find('input')
+    this.$startinput.val(this.start.toFixed(4));
+    this.$endinput.val(this.end.toFixed(4));
+    this.setValue(this.findField("startDuration", this.parent.field.fields), "" + this.start.toFixed(4));
+    this.setValue(this.findField("endDuration", this.parent.field.fields), "" + this.end.toFixed(4));
+  });
+
+  $(self.container).parents('.h5p-craudio-editor').find(".h5p-add-file").first().parent().find('ul').on('DOMSubtreeModified',
+    () => {
+      // let path = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex- 1].params.files ? H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex - 1].params.files[0].path : undefined;
+      let id = H5PEditor.renderableCommonFields["H5P.CRAudio 1.4"].fields[self.crAudioIndex - 1].parent.params.subContentId;
+      let path = self.audioParams.files ? self.audioParams.files[0].path : undefined;
+      if (path != undefined && id != undefined) {
+        let file = H5P.getPath(path, id);
+        $.get(file).done(function () {
+          setTimeout(function () {
+            wavesurfer.load(file);
+          }, 1000)
+        }).fail(function () {
+          let id = H5PEditor.contentId;
+          let file = H5P.getPath(path, id);
+          setTimeout(function () {
+            wavesurfer.load(file);
+          }, 1000)
+        })
+      }
+      if (region != undefined) {
+        let $startinput = $('#' + this.id).parent().parent().find('.field-name-startDuration').find('input');
+        let $endinput = $('#' + this.id).parent().parent().find('.field-name-endDuration').find('input')
+        $startinput.val(0);
+        $endinput.val(0.2);
+        this.setValue(this.findField("startDuration", this.parent.field.fields), "" + 0);
+        this.setValue(this.findField("endDuration", this.parent.field.fields), "" + 0.2);
+        params = {
+          start: 0,
+          end: 0.2
+        }
+        region.update(params)
+      }
+    });
+
+  if (this.id != null) {
+    let regionId = this.id + "playRegion";
+    let $playRegionButton = $('<button id = ' + regionId + ' class = "playRegion">Play</button>');
+    $(self.container).parent('div').append($playRegionButton);
+    $($playRegionButton).on('click', function () {
+      if (region != undefined) {
+        region.play();
+      }
+    })
+  }
+}
 
 /**
  * Append the field to the wrapper.
@@ -236,6 +238,7 @@ WaveformInit.prototype.appendTo = function ($wrapper) {
     }
   })
   self.setValue(self.findField("text", self.parent.field.fields), "" + this.parent.params.text);
+  self.init();
 };
 
 WaveformInit.prototype.findField = function (name, fields) {
